da576a88c79cdeaf30695091af9dbb64aac831bf
Paul <disk_91@hotmail.com>
2026-08-28 18:58:54 +0200

    fix db elments

--- configuration/postgresql/initdb/001-init-chirpstack.sh
+++ configuration/postgresql/initdb/001-init-chirpstack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "chirpstack1ch" <<-EOSQL
-    create role chirpstack1ch with login password 'chirpstack1ch**';
-    create database chirpstack with owner chirpstack1ch;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    create role chirpstack with login password 'chirpstack1ch**';
+    create database chirpstack with owner chirpstack;
 EOSQL
