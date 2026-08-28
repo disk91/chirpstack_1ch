43492078303a84e762a2d754b9bb2c671f1aee12
Paul <disk_91@hotmail.com>
2026-08-28 18:32:48 +0200

    fix db username

--- configuration/postgresql/initdb/001-init-chirpstack.sh
+++ configuration/postgresql/initdb/001-init-chirpstack.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "chirpstack1ch" <<-EOSQL
-    create role chirpstack with login password 'chirpstack1ch**';
-    create database chirpstack with owner chirpstack;
+    create role chirpstack1ch with login password 'chirpstack1ch**';
+    create database chirpstack with owner chirpstack1ch;
 EOSQL
